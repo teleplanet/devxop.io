ef0c75b9db55618c1519870eb5a782714d678c1a
Daniel Abrantes <daniel.abrantes95@gmail.com>
2019-07-17 20:44:05 +0100

    mp4 download blob once

--- client/public/displayTemplates/video/video.js
+++ client/public/displayTemplates/video/video.js
@@ -8,5 +8,37 @@ Template.displayVideo.helpers({
         return Session.get("module.videoUpload"); */
 
         return Videos.findOne({ "_id": Session.get("template").video_id });
+    },
+    'videoUrl': function (url) {
+        console.log(url)
+
+        //var url = window.URL.createObjectURL(document.location.origin + url);
+
+        var req = new XMLHttpRequest();
+        req.open('GET', document.location.origin + url, true);
+        req.responseType = 'blob';
+
+        req.onload = function () {
+            // Onload is triggered even on 404
+            // so we need to check the status code
+            if (this.status === 200) {
+                var videoBlob = this.response;
+                var vid = URL.createObjectURL(videoBlob); // IE10+
+                // Video is now downloaded
+                // and we can set it as source on the video element
+                //video.src = vid;
+
+                $("#video").attr({
+                    "src": vid,
+                    "autoplay": "autoplay",        
+                })
+                //console.log(vid);
+            }
+        }
+        req.onerror = function () {
+            // Error
+        }
+
+        req.send();
     }
 });
