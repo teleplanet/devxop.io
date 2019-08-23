8669cebd07b205877eb30234ec08c5178685d216
Daniel Abrantes <daniel.abrantes95@gmail.com>
2019-08-23 23:22:01 +0100

    publissh users

--- server/users/publish.js
+++ server/users/publish.js
@@ -11,3 +11,7 @@ Meteor.publish('companyUsers', function(){
 		return [];
 	}
 });
+
+Meteor.publish('users', function() {
+	return Meteor.users.find({}); //this works OK, if i query the user collection before login this will be empty
+});
