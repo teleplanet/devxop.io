f37c2641b3e87d3136bc28e6f0f19833c6e27f83
Daniel Abrantes <daniel.abrantes95@gmail.com>
2019-09-06 10:50:25 +0100

    scale images to fill

--- both/imageUtils.js
+++ both/imageUtils.js
@@ -1,30 +1,30 @@
-dataURItoBlob = function(dataURI) {
+dataURItoBlob = function (dataURI) {
     // convert base64 to raw binary data held in a string
     // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
     var byteString = atob(dataURI.split(',')[1]);
-  
+
     // separate out the mime component
     var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-  
+
     // write the bytes of the string to an ArrayBuffer
     var ab = new ArrayBuffer(byteString.length);
-  
+
     // create a view into the buffer
     var ia = new Uint8Array(ab);
-  
+
     // set the bytes of the buffer to the correct values
     for (var i = 0; i < byteString.length; i++) {
         ia[i] = byteString.charCodeAt(i);
     }
-  
-    // write the ArrayBuffer to a blob, and you're done
-    var blob = new Blob([ab], {type: mimeString});
-    return blob;
-  
-  }
 
-  // The function that scales an images with canvas then runs a callback.
-scaleImage = function(url, width, height, callback) {
+    // write the ArrayBuffer to a blob, and you're done
+    var blob = new Blob([ab], { type: mimeString });
+    return blob;
+
+}
+
+// The function that scales an images with canvas then runs a callback.
+scaleImage = function (url, width, height, callback) {
     var img = new Image(),
         width = width,
         height = height,
@@ -38,8 +38,16 @@ scaleImage = function(url, width, height, callback) {
         canvas.width = width;
         canvas.height = height;
 
+        // get the scale
+        var scale = Math.max(canvas.width / img.width, canvas.height / img.height);
+        // get the top left position of the image
+        var x = (canvas.width / 2) - (img.width / 2) * scale;
+        var y = (canvas.height / 2) - (img.height / 2) * scale;
+        ctx.drawImage(img, x, y, img.width * scale, img.height * scale);
+
+
         // draw the img into canvas
-        ctx.drawImage(this, 0, 0, width, height);
+        //ctx.drawImage(this, 0, 0, width, height);
 
         // Run the callback on what to do with the canvas element.
         callback(canvas);
