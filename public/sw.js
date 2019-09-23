b450df9132bec1d2e0fdd13cae95bed4295e50b9
Daniel Abrantes <daniel.abrantes95@gmail.com>
2019-09-23 16:50:12 +0100

    sw

---
+++ public/sw.js
@@ -0,0 +1,63 @@
+'use strict';
+
+/* eslint-disable max-len */
+
+const applicationServerPublicKey = 'BA477LIzpRITyZ83BaNVX5mjUOiNok2p0Kt9k7elV8sjmtro_kfwpcdVcD5JxVEGNyW5-P1QRny2n-K4GGodSi0';
+
+/* eslint-enable max-len */
+
+function urlB64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
+
+self.addEventListener('push', function (event) {
+    console.log(event.data.text());
+
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+    const title = "Pager.io (G's Bistro)";
+    const options = {
+        body: "Pager Alarm! Your food is ready",
+        icon: 'images/icon.png',
+        badge: 'images/badge.png'
+    };
+
+    event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function (event) {
+    console.log('[Service Worker] Notification click Received.');
+
+    event.notification.close();
+
+    event.waitUntil(
+        clients.openWindow('https://developers.google.com/web/')
+    );
+});
+
+self.addEventListener('pushsubscriptionchange', function (event) {
+    console.log('[Service Worker]: \'pushsguk9ubscriptionchange\' event fired.');
+    const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+    event.waitUntil(
+        self.registration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: applicationServerKey
+        })
+            .then(function (newSubscription) {
+                // TODO: Send to application server
+                console.log('[Service Worker] New subscription: ', newSubscription);
+            })
+    );
+});
