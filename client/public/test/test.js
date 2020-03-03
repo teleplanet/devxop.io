a83068a696e65bd62910a2387dd8222e6a0462b8
Daniel Abrantes <daniel.abrantes95@gmail.com>
2020-03-03 21:54:01 +0000

    imagetext multiple dev

--- client/public/test/test.js
+++ client/public/test/test.js
@@ -3,6 +3,21 @@
 // Created a plugin for project portability
 
 
+function createMarkup(obj) {
+    var keys = Object.keys(obj)
+    if (!keys.length) return ''
+    var i, len = keys.length
+    var result = ''
+
+    for (i = 0; i < len; i++) {
+        var key = keys[i]
+        var val = obj[key]
+        result += key + ':' + val + ';'
+    }
+
+    return result
+}
+
 
 Template.displayImageText.onRendered(function () {
     Tracker.autorun(function () {
@@ -10,16 +25,22 @@ Template.displayImageText.onRendered(function () {
 
         if (!template) {
             TemplatesImageText.insert({
-                "image_url": "",
-                "image_data": "",
-                "style": {},
-                "value": {},
-                "text": "",
-                "text2": "",
+                "image": "http://localhost:3000/cfs/files/images/J4XFkuh6XcTgfwonM?token=eyJhdXRoVG9rZW4iOiJ3Z2hIUjd6ZDJ3Uk5nUkxtZS1SYmlZSW8zQ2p6dnphOXVnQmJrNExsNFFNIn0%3D?1583141170158",
+                "texts": [{
+                    "value": "this is string 1",
+                    "style": {},
+                    "originals": {},
+                },{
+                    "value": "this is string 1",
+                    "style": {},
+                    "originals": {},
+                }],
 
             });
         } else {
-            $("#image-text").cssMap(template.style);
+            //Session.set("text-index", template.text_index);
+            //$("#image-text").cssMap(template.style);
+            //log(createMarkup(template.style));
         }
     });
 });
@@ -31,75 +52,121 @@ Template.displayImageText.onRendered(function () {
  */
 
 Template.displayImageText.helpers({
-    "text": function(){
-        return TemplatesImageText.findOne()["text"];
+    "template": function () {
+        return TemplatesImageText.findOne();
     },
-    "text2": function(){
+    "text2": function () {
         return TemplatesImageText.findOne()["text2"];
+    },
+    "parseStyle": function(style){
+        return createMarkup(style);
+    }
+});
+
+Template.displayImageText.events({
+    'click .js-select-text': function(event){
+        let template = TemplatesImageText.findOne();
+        let index = $(event.target).data("index");
+
+        TemplatesImageText.update(template._id, {
+            $set: {
+                "text_index": index,
+            }
+        });
     },
 });
 
- Template.test.helpers({
-    "text": function(){
-        return TemplatesImageText.findOne()["text"];
-    },
-    "text2": function(){
-        return TemplatesImageText.findOne()["text2"];
-    },
-    "style": function(key){
-        if(key){
-            return TemplatesImageText.findOne().style[key];
-        }else{
-            return TemplatesImageText.findOne().style
+Template.test.helpers({
+    "style": function (key) {
+        let template = TemplatesImageText.findOne();
+        let index = template.text_index;
+        if (key) {
+            return TemplatesImageText.findOne().texts[index].style[key];
+        } else {
+            return TemplatesImageText.findOne().texts[index].style;
         }
-        
+
     },
-    "value": function(key){
-        if(key){
-            return TemplatesImageText.findOne().value[key];
-        }else{
-            return TemplatesImageText.findOne().value
+    "value": function (key) {
+        let template = TemplatesImageText.findOne();
+        let index = template.text_index;
+        if (key) {
+            return TemplatesImageText.findOne().texts[index].originals[key];
+        } else {
+            return TemplatesImageText.findOne().texts[index].originals
         }
-        
-    }
+
+    },
+    "texts": function(){
+        return TemplatesImageText.findOne().texts;
+    },
+    "textSelected": function(){
+        let template = TemplatesImageText.findOne();
+        let index = template.text_index;
+        return TemplatesImageText.findOne().texts[index];
+    },
 });
+
 
 Template.test.events({
     'change .js-edit-text': function (event) {
         let template = TemplatesImageText.findOne();
+        let index = template.text_index;
         let val = $(event.target).val();
-        let key = $(event.target).data("key");
+
 
         if (template) {
 
-            let text = {};
-            text[key] = val;
+            let text = template.texts[index];
+            text["value"] = val;
+
+            template.texts[index] = text;
 
             TemplatesImageText.update(template._id, {
-                $set: text
+                $set: {
+                    "texts": template.texts,
+                }
             });
 
         }
     },
     'change .js-imageText-edit': function (event) {
         let template = TemplatesImageText.findOne();
+        let index = template.text_index;
         let val = $(event.target).val();
         let key = $(event.target).data("key");
         let unit = $(event.target).data("unit");
 
+        console.log(event);
+        console.log(val);
 
         if (template) {
 
-            let style = template.style;
+
+            if($(event.target).is(':checkbox') && $(event.target).is(':checked')){
+                console.log("is checkbox and cehecked");
+            }
+
+            let text = template.texts[index];
+            let style = text.style;
             style[key] = val + unit;
 
-            let value = template.value;
-            value[key] = val;
+            if(key == "padding"){
+                let newVal = val + "vh " + (val*2) + "vw";
+                style[key] = newVal;
+            }
+
+            let originals = text.originals;
+            originals[key] = val;
+
+            text["originals"] = originals;
+            text["style"] = style;
+
+            template.texts[index] = text;
 
             TemplatesImageText.update(template._id, {
                 $set: {
-                    "style": style,
-                    "value": value
+                    "texts": template.texts,
                 }
             });
 
@@ -107,17 +174,26 @@ Template.test.events({
     },
     'change #imageText-color': function () {
         let template = TemplatesImageText.findOne();
+        let index = template.text_index;
         let val = $('#imageText-color option:selected').val();
 
 
         if (template) {
 
-            let style = template.style;
+            let text = template.texts[index];
+            let style = text.style;
             style["color"] = val;
+            let originals = text.originals;
+            originals["color"] = val;
+
+            text["originals"] = originals;
+            text["style"] = style;
+
+            template.texts[index] = text;
 
             TemplatesImageText.update(template._id, {
                 $set: {
-                    "style": style
+                    "texts": template.texts,
                 }
             });
 
